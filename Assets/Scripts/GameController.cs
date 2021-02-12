3cfc6f33910098ce243f282cc8f0b3382783b127
Tuan Pham <tuan.programmer0101@gmail.com>
2021-02-12 15:33:46 -0500

    Create GameController.cs

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    enum Piece
+    {
+        Empty = 0, Yellow = 1, Red = 2
+    }
+
+    //public GameObject boardObject;
+    public GameObject pieceRed;
+    public GameObject pieceYellow;
+
+    public Board board;
+
+    GameObject currentPiece;
+
+    bool allowMousePress = true;
+    //bool gameOver = false;
+    bool isPlayerTurn = false;
+    bool isDroping = false;
+
+    int col;
+    Vector3 startPos;
+    Vector3 endPos;
+}
