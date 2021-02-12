6c171b709d3bd5f6e3ca14d32af7d137563079fe
Tuan Pham <tuan.programmer0101@gmail.com>
2021-02-12 15:35:10 -0500

    Spawn Piece

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -9,10 +9,8 @@ public class GameController : MonoBehaviour
         Empty = 0, Yellow = 1, Red = 2
     }
 
-    //public GameObject boardObject;
     public GameObject pieceRed;
     public GameObject pieceYellow;
-
     public Board board;
 
     GameObject currentPiece;
@@ -25,4 +23,21 @@ public class GameController : MonoBehaviour
     int col;
     Vector3 startPos;
     Vector3 endPos;
+
+    void Start()
+    {
+        currentPiece = SpawnPiece();
+    }
+
+    GameObject SpawnPiece()
+    {
+        Vector3 spawnPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        GameObject newPiece = Instantiate(//pieceYellow,new Vector3(1,1,1),
+            isPlayerTurn ? pieceYellow : pieceRed, // if it's player turn, get yellow piece, else red
+            new Vector3(Mathf.Clamp(spawnPos.x, 0, board.GetColums() - 1), 1, 1),
+            Quaternion.identity) as GameObject;
+
+        return newPiece;
+    }
 }
